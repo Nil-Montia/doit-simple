398947518db7eaecb97016abdcecef1aeb5a58a2
Nil Montia <nil.montia@academytrainee.com>
2019-05-01 13:38:24 +0100

    added delete and update functionality, but there currently is a clash between them

--- src/App.js
+++ src/App.js
@@ -4,75 +4,98 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from "./Navbar";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: "something",
-      arr: [],
-      id:1
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: "something",
+            arr: []
+        }
     }
-  }
 
-  loadListItems = () =>{
-    const request = new XMLHttpRequest();
-    const url='http://localhost:8080/task/getAll';
-    request.open("GET", url);
-    request.responseType='json';
-    request.setRequestHeader("content-Type","application/json")
-    request.onload=(e)=>{
-      let list=request.response;
-      this.setState({
-        arr:list
-      })
+    loadListItems = () => {
+        const request = new XMLHttpRequest();
+        const url = 'http://localhost:8080/task/getAll';
+        request.open("GET", url);
+        request.responseType = 'json';
+        request.setRequestHeader("content-Type", "application/json")
+        request.onload = (e) => {
+            let list = request.response;
+            console.log("list: ", list);
+            this.setState({
+                arr: list
+            })
+        };
+        request.send();
     };
-    request.send();
-  };
 
-  componentDidMount() {
-    this.loadListItems();
-  }
+    componentDidMount() {
+        this.loadListItems();
+    }
 
-  create = (text) => {
-    const request = new XMLHttpRequest();
-    const url='http://localhost:8080/task/create';
-    request.open("POST", url);
-    request.responseType='json';
-    request.setRequestHeader("content-Type","application/json")
-    let body=JSON.stringify({description:text})
-    request.onload=(e)=>{
-      console.log(request.status)
-      this.loadListItems()
-    };
-    request.send(body);
-  }
+    create = (text) => {
+        const request = new XMLHttpRequest();
+        const url = 'http://localhost:8080/task/create';
+        request.open("POST", url);
+        request.responseType = 'json';
+        request.setRequestHeader("content-Type", "application/json")
+        let body = JSON.stringify({description: text})
+        request.onload = (e) => {
+            console.log(request.status)
+            this.loadListItems()
+        };
+        request.send(body);
+    }
 
-  update = (id, text) =>{
-      const request = new XMLHttpRequest();
-      const url=`http://localhost:8080/task/delete/${id}`;
-      request.open("DELETE", url);
-      request.responseType='json';
-      request.setRequestHeader("content-Type","application/json")
+    update = (id, text) => {
+        const request = new XMLHttpRequest();
+        const url = `http://localhost:8080/task/update/${id}`;
+        request.open("POST", url);
+        request.responseType = 'json';
+        request.setRequestHeader("content-Type", "application/json");
+        let body = JSON.stringify({description: text});
+        request.onload = (e) => {
+            console.log(request.status)
+            this.loadListItems()
+        };
+        request.send(body);
+    }
 
-  }
+    delete = (id) => {
+        const request = new XMLHttpRequest();
+        const url = `http://localhost:8080/task/delete/${id}`;
+        request.open("DELETE", url);
+        request.responseType = 'json';
+        request.setRequestHeader("content-Type", "application/json");
+        request.setRequestHeader("Accept", "application/json");
+        request.onload = () => {
+            console.log("status: ", request.status);
+            this.loadListItems()
+        };
+        request.send();
+    }
 
-  show = () => {
-    this.create(this.state.text);
-  }
+    transcribe = (e) => {
+        this.setState({
+            text: e.target.value
+        })
+    }
 
-  transcribe = (e) => {
-    this.setState({
-      text: e.target.value
-    })
-  }
-
-  render() {
-    return (<div className={"card"}>
-      <Navbar/>
-      <input className={"form-control"} type={"text"} placeholder={"Enter task"} onChange={this.transcribe}/>
-      <button type={"submit"} onClick={this.show}>Submit</button>
-      {this.state.arr.map((item, index) => <ListItem className={"self-align-center"} key={"item"+index} text={item.description} taskID={item.id} update={this.update}/>)}
-    </div>);
-  }
+    render() {
+        console.log("Array in render: ", this.state.arr);
+        return (
+            <div className={"card"}>
+                <Navbar/>
+                <input className={"form-control"} type={"text"} placeholder={"Enter task"} onChange={this.transcribe}/>
+                <button type={"submit"} onClick={() => {
+                    this.create(this.state.text)
+                }}>Submit
+                </button>
+                {this.state.arr.map((item, index) => <ListItem className={"self-align-center"} key={"item" + index}
+                                                               text={item.description} taskID={item.id}
+                                                               update={this.update}
+                                                               delete={this.delete} sayHi={this.sayHi}/>)}
+            </div>);
+    }
 }
 
 export default App;
--- src/ListItem.js
+++ src/ListItem.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-class ListItem extends Component{
-    constructor(props){
+
+class ListItem extends Component {
+    constructor(props) {
         super(props);
-        this.state={
-            text: props.text||"whoops",
-            isInput:false
+        this.state = {
+            text: props.text || "whoops",
+            isInput: false
         };
     };
+
     display = (e) => {
-        if (e.key==="Enter"){
+        if (e.key === "Enter") {
+            this.props.update(this.props.taskID, this.state.text)
             this.setState({
                 isInput: false
             })
@@ -17,16 +20,28 @@ class ListItem extends Component{
     };
 
     transcribe = (e) => {
+        console.log("b")
+
         this.setState({
-            text:e.target.value
+            text: e.target.value
         })
     };
 
-    render(){
-        let item= <div onClick={()=>{this.setState({isInput:true})}}>{this.state.text}<button className={"button btn-small btn-danger mr-auto"}>-</button></div>;
-        let input=<input type={"text"} className={"form-control"} placeholder={"Input description"} onChange={this.transcribe} onKeyDown={this.display} value={this.state.text}/>;
-        return(<div>
-            {!this.isInput ? item : input}
+    render() {
+        console.log(this.props.taskID);
+
+        let item = <div>
+                <span onClick={() => {this.setState({isInput: true})}}>{this.state.text}</span>
+                <button className={"button btn-small btn-danger mr-auto"}
+                        onClick={() => {this.props.delete(this.props.taskID)}}>
+                 -
+                </button>
+        </div>;
+
+        let input = <input type={"text"}className={"form-control"} placeholder={"Input description"}
+                           onChange={this.transcribe} onKeyDown={this.display} value={this.state.text}/>;
+        return (<div>
+            {!this.state.isInput ? item : input}
         </div>)
     }
 }
